42014e4f2ef3f29909c413e3e63a4aacdd1e8c56
zanostro <63290391+zanostro@users.noreply.github.com>
2025-12-29 17:45:06 +0100

    updated size

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,7 +46,7 @@ namespace SensorSimulatorGui
             panelNav.Location = new Point(0, 0);
             panelNav.Name = "panelNav";
             panelNav.Padding = new Padding(10, 14, 10, 10);
-            panelNav.Size = new Size(160, 624);
+            panelNav.Size = new Size(160, 688);
             panelNav.TabIndex = 1;
             // 
             // btnNavArchived
@@ -107,7 +107,7 @@ namespace SensorSimulatorGui
             panelContent.Location = new Point(160, 0);
             panelContent.Name = "panelContent";
             panelContent.Padding = new Padding(14, 14, 14, 10);
-            panelContent.Size = new Size(937, 624);
+            panelContent.Size = new Size(1231, 688);
             panelContent.TabIndex = 0;
             // 
             // statusStrip
@@ -115,9 +115,9 @@ namespace SensorSimulatorGui
             statusStrip.BackColor = Color.FromArgb(17, 24, 39);
             statusStrip.ImageScalingSize = new Size(20, 20);
             statusStrip.Items.AddRange(new ToolStripItem[] { lblEndpoints });
-            statusStrip.Location = new Point(0, 624);
+            statusStrip.Location = new Point(0, 688);
             statusStrip.Name = "statusStrip";
-            statusStrip.Size = new Size(1097, 26);
+            statusStrip.Size = new Size(1391, 26);
             statusStrip.SizingGrip = false;
             statusStrip.TabIndex = 2;
             // 
@@ -125,7 +125,7 @@ namespace SensorSimulatorGui
             // 
             lblEndpoints.ForeColor = Color.FromArgb(226, 232, 240);
             lblEndpoints.Name = "lblEndpoints";
-            lblEndpoints.Size = new Size(1082, 20);
+            lblEndpoints.Size = new Size(1376, 20);
             lblEndpoints.Spring = true;
             lblEndpoints.Text = "Endpoints: (loaded from appsettings.json)";
             // 
@@ -134,7 +134,7 @@ namespace SensorSimulatorGui
             AutoScaleDimensions = new SizeF(9F, 23F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(30, 41, 59);
-            ClientSize = new Size(1097, 650);
+            ClientSize = new Size(1391, 714);
             Controls.Add(panelContent);
             Controls.Add(panelNav);
             Controls.Add(statusStrip);
